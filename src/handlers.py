1ea931a0d1c6e18433cc35463d23197578de2d98
Mysti <miomer@yahoo.co.uk>
2023-03-23 09:24:30 +0000

    remove debug message

--- src/handlers.py
+++ src/handlers.py
@@ -4,12 +4,6 @@ from sql import db_exists, create_database
 def handle_create_database (spec):
     target_server = spec["server"]
     target_database = _parse_spec_database_name(spec["db"]["name"])
-    _log_message('info', f"""
-#spec yaml
-spec: 
-  server: {target_server}
-  db:
-    name: {target_database}""")
     if db_exists(target_server, target_database):
         _log_message('warning', f"Target database {target_database} already exists on {target_server}")
     else:
